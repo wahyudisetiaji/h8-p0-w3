db46d1e35003d98861494599d436b4ef281de74f
Wahyudi Setiaji <wahyudisetiaji@yahoo.com>
2018-02-09 21:51:59 +0700

    Update Exercises 3 Week 3

--- exercises3week3.js
+++ exercises3week3.js
@@ -1,64 +1,39 @@
-// Menggunakan Built-in Function pada Array
+// ARRAY - IF ELSE
 
-var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"];
+var input = [
+                ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+                ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+                ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+                ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+            ];
 
-function dataHandling2(){
- // UPDATE DATA
- var nama = input[1].split(" ")
- var provinsi = input[2].split(" ")
 
- nama.push("Elsharawy")
- provinsi.unshift("Provinsi")
-
- input.splice(1,1, nama.join(" "))
- input.splice(2,1, provinsi.join(" "))
- input.splice(4,1, "Pria", "SMA Internasional Metro")
- console.log(input)
-
- // BULAN
- var month = input[3].split("/")
- switch(month[1]){
-    case '01' : {console.log("Januari"); break;}
-    case '02' : {console.log("Februari"); break;}
-    case '03' : {console.log("Maret"); break;}
-    case '04' : {console.log("April"); break;}
-    case '05' : {console.log("Mei"); break;}
-    case '06' : {console.log("Juni"); break;}
-    case '07' : {console.log("Juli"); break;}
-    case '08' : {console.log("Agustus"); break;}
-    case '09' : {console.log("September"); break;}
-    case '10' : {console.log("Oktober"); break;}
-    case '11' : {console.log("November"); break;}
-    case '12' : {console.log("Desember"); break;}
- }
-
- // EDIT POSISI
- var tanggal = input[3].split("/")
- tanggal.sort(function(a,b){return b - a});
- console.log(tanggal)
-
- // FORMAT dd-mm-YYY
- var format = input[3].split("/")
- var tanggall = format[0].split("")
- var bulan = format[1].split("")
- var tahun = format[2].split("")
- bulan.unshift("-")
- tahun.unshift("-")
- tanggall = tanggall.join("")
- bulan = bulan.join("")
- tahun = tahun.join("")
-
- var formatTgl = tanggall+bulan+tahun
-
- console.log(formatTgl)
-
- // EDIT NAMA
-
- var editNama = input[1].split(" ")
- editNama.pop()
- editNama = editNama.join(" ")
- console.log(editNama)
-
+function dataHandling(nomorID){
+  if (nomorID === "0001"){
+    console.log("Nomor ID : " + input[0][0]);
+    console.log("Nama Lengkap : " + input[0][1]);
+    console.log("TTL : " + input[0][2] + " " + input[0][3]);
+    console.log("Hobi : " + input[0][4]);
+    return
+  }else if (nomorID === "0002"){
+    console.log("Nomor ID : " + input[1][0]);
+    console.log("Nama Lengkap : " + input[1][1]);
+    console.log("TTL : " + input[1][2] + " " + input[1][3]);
+    console.log("Hobi : " + input[1][4]);
+    return
+  }else if (nomorID === "0003"){
+    console.log("Nomor ID : " + input[2][0]);
+    console.log("Nama Lengkap : " + input[2][1]);
+    console.log("TTL : " + input[2][2] + " " + input[2][3]);
+    console.log("Hobi : " + input[2][4]);
+    return
+  }else if (nomorID == "0004"){
+    console.log("Nomor ID : " + input[3][0]);
+    console.log("Nama Lengkap : " + input[3][1]);
+    console.log("TTL : " + input[3][2] + " " + input[3][3]);
+    console.log("Hobi : " + input[3][4]);
+    return
+  }
 }
 
-dataHandling2();
+dataHandling("0004")
