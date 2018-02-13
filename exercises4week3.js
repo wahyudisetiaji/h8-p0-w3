075c1db06e1b7c50d855b3bdbd6cc6118d713207
Wahyudi Setiaji <wahyudisetiaji@yahoo.com>
2018-02-13 20:21:18 +0700

    Update Exercies 4

--- exercises4week3.js
+++ exercises4week3.js
@@ -17,46 +17,48 @@ function dataHandling2(){
 
  // BULAN
  var month = input[3].split("/")
- switch(month[1]){
-    case '01' : {console.log("Januari"); break;}
-    case '02' : {console.log("Februari"); break;}
-    case '03' : {console.log("Maret"); break;}
-    case '04' : {console.log("April"); break;}
-    case '05' : {console.log("Mei"); break;}
-    case '06' : {console.log("Juni"); break;}
-    case '07' : {console.log("Juli"); break;}
-    case '08' : {console.log("Agustus"); break;}
-    case '09' : {console.log("September"); break;}
-    case '10' : {console.log("Oktober"); break;}
-    case '11' : {console.log("November"); break;}
-    case '12' : {console.log("Desember"); break;}
+ var bulanInteger = parseInt(month[1])
+ switch(bulanInteger){
+    case 1 : { bulanInteger = "Januari"; break;}
+    case 2 : { bulanInteger = "Februari"; break;}
+    case 3 : { bulanInteger = "Maret"; break;}
+    case 4 : { bulanInteger = "April"; break;}
+    case 5 : { bulanInteger = "Mei"; break;}
+    case 6 : { bulanInteger = "Juni"; break;}
+    case 7 : { bulanInteger = "Juli"; break;}
+    case 8 : { bulanInteger = "Agustus"; break;}
+    case 9 : { bulanInteger = "September"; break;}
+    case 10 : { bulanInteger = "Oktober"; break;}
+    case 11 : { bulanInteger = "November"; break;}
+    case 12 : { bulanInteger = "Desember"; break;}
  }
+
+ console.log(bulanInteger)
 
  // EDIT POSISI
  var tanggal = input[3].split("/")
- tanggal.sort(function(a,b){return b - a});
- console.log(tanggal)
+  tanggal.sort(function(a,b){return b - a});
+  console.log(tanggal)
 
- // FORMAT dd-mm-YYY
- var format = input[3].split("/")
- var tanggall = format[0].split("")
- var bulan = format[1].split("")
- var tahun = format[2].split("")
- bulan.unshift("-")
- tahun.unshift("-")
- tanggall = tanggall.join("")
- bulan = bulan.join("")
- tahun = tahun.join("")
+// // FORMAT dd-mm-YYY
+  var format = input[3].split("/")
+  var tanggall = format[0].split("")
+  var bulan = format[1].split("")
+  var tahun = format[2].split("")
+  bulan.unshift("-")
+  tahun.unshift("-")
+  tanggall = tanggall.join("")
+  bulan = bulan.join("")
+  tahun = tahun.join("")
 
- var formatTgl = tanggall+bulan+tahun
+  var formatTgl = tanggall+bulan+tahun
 
- console.log(formatTgl)
+  console.log(formatTgl)
 
  // EDIT NAMA
 
- var editNama = input[1].split(" ")
- editNama.pop()
- editNama = editNama.join(" ")
+ var editNama = input[1].toString()
+ editNama = editNama.slice(0,15)
  console.log(editNama)
 
 }
